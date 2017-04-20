393a7200f82f0bf5760c22e97f46e1a8ddb9d8c4
PeacefulAlien <sun.zao@me.com>
2017-04-20 16:54:55 +0200

    added description of each bascia info, backup before sign out

--- Object_Basic_info.py
+++ Object_Basic_info.py
@@ -1,5 +1,13 @@
 """
-This object will define the basic employee information and structure for the information
+This object will define the basic employee information
+ID: unique four digits identification number of each individual, integer 
+first: first name, string
+last: last name, string
+phone_number: cellphone number for SMS reminder feature, integer
+MAC: MAC address for each individual's working computer, string
+fullname: full name for display feature, string
+email: email address for email reminder feature, string
+*...
 """
 
 
@@ -8,12 +16,13 @@ import csv
 
 class employee():
 
-	def __init__(self, ID, first, last, phone_number):
+	def __init__(self, ID, first, last, phone_number, MAC):
 		self.ID = []
 		self.first = []
 		self.last = []
 		self.phone_number = []
-	
+		self.MAC = []
+		
 	@property
 	def fullname(self):
 		return '{} {}'.format(self.first, self.last)
@@ -24,7 +33,8 @@ class employee():
 	
 	def employee_basic_info_load(self):
 		with open('employee_basic_info.csv') as input_file:
-			
+			if csv.Sniff.has_header(input_file):
+				
 		
 			employee_basic_info = csv.reader(input_file, delimiter=';')
 			for row in employee_basic_info:
